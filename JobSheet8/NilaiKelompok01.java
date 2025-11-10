276e64b35324c39977e698b934356205043ebdaa
pepksyyy <ahamadrifqi13@gmail.com>
2025-11-10 17:47:12 +0700

    modif kelompok terbaik

--- JobSheet8/NilaiKelompok01.java
+++ JobSheet8/NilaiKelompok01.java
@@ -1,24 +1,44 @@
 import java.util.Scanner;
+
 public class NilaiKelompok01 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        double nilai = 0, totalNilai = 0, rataNilai = 0;
-        int i = 1;
+        Scanner sc = new Scanner(System.in);
 
-    while (i <= 6){
-        System.out.println("Kelompok " + i);
-        totalNilai = 0;
-    
-        for (int j = 1; j <= 5; j++){
-            System.out.print("Masukkan nilai ke-" + j + " = ");
-            nilai = sc.nextDouble();
-            totalNilai += nilai;
-        }
+        double nilai;
+        double totalNilai;
+        double rataNilai;
+        int i = 1; 
 
-        rataNilai = totalNilai / 5;
-        System.out.println("Rata-rata nilai kelompok " + i + " = " + rataNilai);
-        i++;
+        double maxRataRata = -1; 
+        int kelompokTerbaik = 0; 
+
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+            totalNilai = 0; 
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("Masukkan nilai ke-" + j + ": ");
+                nilai = sc.nextDouble();
+                totalNilai += nilai;
+            } 
+
+            rataNilai = totalNilai / 5;
+
+            if (rataNilai > maxRataRata) {
+                maxRataRata = rataNilai;
+                kelompokTerbaik = i; 
+            }
+
+            System.out.println("Rata-rata: " + rataNilai);
+            System.out.println("--------------------");
+
+            i++; 
+        } 
+
+        System.out.println("Kelompok dengan Rata-rata Nilai Tertinggi adalah:");
+        System.out.println("Nomor Kelompok: " + kelompokTerbaik);
+        System.out.println("Rata-rata Tertinggi: " + maxRataRata);
+        
+        sc.close();
     }
-    sc.close();
-}
 }

9b401d85f1e86b521ce511ba481f0e9aa1138551
pepksyyy <ahamadrifqi13@gmail.com>
2025-12-09 10:40:47 +0700

    Tugas 1

---
+++ JobSheet11/Kubus01.java
@@ -0,0 +1,20 @@
+public class Kubus01 {
+    public static double hitungVolume(double sisi) {
+        return sisi * sisi * sisi;
+    }
+
+    public static double hitungLuasPermukaan(double sisi) {
+        return 6 * sisi * sisi;
+    }
+
+    public static void main(String[] args) {
+        double sisi = 5.0;
+
+        double volume = hitungVolume(sisi);
+        double luasPermukaan = hitungLuasPermukaan(sisi);
+
+        System.out.println("Sisi Kubus: " + sisi);
+        System.out.println("Volume Kubus: " + volume);
+        System.out.println("Luas Permukaan Kubus: " + luasPermukaan);
+    }
+}
